8cf32bdc9923ef35cac2cd6604580612c1716cf3
yensi23 <51719424+yensi23@users.noreply.github.com>
2022-11-30 11:09:59 +0100

    Add files via upload

---
+++ GoogleTest4CookiesConsent.java
@@ -0,0 +1,48 @@
+package com.training.selenium;
+
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.Dimension;
+
+
+
+public class GoogleTest4CookiesConsent {
+  private static WebDriver driver;
+  
+  
+  @BeforeAll
+  static public void setUp() {	
+		driver = new ChromeDriver();
+	
+  }
+  
+  @AfterAll
+  static public void tearDown() {
+	  //if (driver != null)
+		  //driver.quit();
+  }
+  
+  
+  @Test
+  public void google() {
+    driver.get("https://www.google.com/");
+	driver.manage().window().setSize(new Dimension(1936, 1056));
+	
+	driver.manage().deleteCookieNamed ("CONSENT");
+	driver.manage().addCookie(new Cookie("CONSENT","YES+shp.gws-"+LocalDate.now().toString().replace("-","")+"-0-RC2.en+FX+374"));
+	driver.navigate().refresh();
+	
+    assertEquals(driver.getTitle(), "Google");
+  }
+  
+}
